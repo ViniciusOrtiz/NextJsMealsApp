7409b721c863a3fbbf0eb3729e29ced29c95b920
Vinicius Ortiz <vinicius-ortiz@outlook.com>
2024-03-21 22:18:18 -0300

    get meals

--- lib/meals.tsx
+++ lib/meals.tsx
@@ -3,7 +3,11 @@ import sql from 'better-sqlite3';
 const db = sql('meals.db');
 
 export async function getMeals() {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     // throw new Error('Failed to fetch meals')
     return db.prepare('SELECT * FROM meals').all();
+}
+
+export async function getMeal(slug: any) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
--- src/app/(pages)/meals/[slug]/page.tsx
+++ src/app/(pages)/meals/[slug]/page.tsx
@@ -1,3 +1,31 @@
-export default function MealsSlugPage() {
-  return <h1>Meals Slug Page</h1>;
+import Image from 'next/image';
+import { getMeal } from '../../../../../lib/meals';
+import styles from './page.module.css';
+
+export default async function MealsSlugPage({params}: any) {
+  var meal = await getMeal(params.slug) as any
+  var instructions = meal.instructions.replace(/\n/g, '<br/>')
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.alt} fill/>
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{
+          __html: instructions
+        }}></p>
+      </main>
+    </>
+  )
 }
