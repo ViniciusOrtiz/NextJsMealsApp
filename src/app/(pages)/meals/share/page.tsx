8fa5d9ee9f438ed45993fcad91417cb3372ce6af
Vinicius Ortiz <vinicius-ortiz@outlook.com>
2024-03-22 07:59:20 -0300

    redirect after submit

--- src/app/(pages)/meals/share/page.tsx
+++ src/app/(pages)/meals/share/page.tsx
@@ -1,52 +1,52 @@
 import MealsImagePicker from '@/components/meals-image-picker/meals-image-picker';
 import styles from './page.module.css';
 import { shareMeal } from '@/lib/action';
+import MealsFormSubmit from '@/components/meals-form-submit/meals-form-submit';
 
 export default function MealsSharePage() {
-
-    return (
-        <>
-          <header className={styles.header}>
-            <h1>
-              Share your <span className={styles.highlight}>favorite meal</span>
-            </h1>
-            <p>Or any other meal you feel needs sharing!</p>
-          </header>
-          <main className={styles.main}>
-            <form className={styles.form} action={shareMeal}>
-              <div className={styles.row}>
-                <p>
-                  <label htmlFor="name">Your name</label>
-                  <input type="text" id="name" name="name" required />
-                </p>
-                <p>
-                  <label htmlFor="email">Your email</label>
-                  <input type="email" id="email" name="email" required />
-                </p>
-              </div>
+  return (
+      <>
+        <header className={styles.header}>
+          <h1>
+            Share your <span className={styles.highlight}>favorite meal</span>
+          </h1>
+          <p>Or any other meal you feel needs sharing!</p>
+        </header>
+        <main className={styles.main}>
+          <form className={styles.form} action={shareMeal}>
+            <div className={styles.row}>
               <p>
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" required />
+                <label htmlFor="name">Your name</label>
+                <input type="text" id="name" name="name" required />
               </p>
               <p>
-                <label htmlFor="summary">Short Summary</label>
-                <input type="text" id="summary" name="summary" required />
+                <label htmlFor="email">Your email</label>
+                <input type="email" id="email" name="email" required />
               </p>
-              <p>
-                <label htmlFor="instructions">Instructions</label>
-                <textarea
-                  id="instructions"
-                  name="instructions"
-                  rows={10}
-                  required
-                ></textarea>
-              </p>
-              <MealsImagePicker label='Your Image' name='image'/>
-              <p className={styles.actions}>
-                <button type="submit">Share Meal</button>
-              </p>
-            </form>
-          </main>
-        </>
-      );
+            </div>
+            <p>
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" name="title" required />
+            </p>
+            <p>
+              <label htmlFor="summary">Short Summary</label>
+              <input type="text" id="summary" name="summary" required />
+            </p>
+            <p>
+              <label htmlFor="instructions">Instructions</label>
+              <textarea
+                id="instructions"
+                name="instructions"
+                rows={10}
+                required
+              ></textarea>
+            </p>
+            <MealsImagePicker label='Your Image' name='image'/>
+            <p className={styles.actions}>
+              <MealsFormSubmit/>
+            </p>
+          </form>
+        </main>
+      </>
+  );
 }
