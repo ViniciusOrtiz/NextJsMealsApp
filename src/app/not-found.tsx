8bae68375d7fa64e15ffb4b3d405122a07ec83ee
Vinicius Ortiz <vinicius-ortiz@outlook.com>
2024-03-21 22:04:44 -0300

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundError() {
+    return (
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Unfortunately, we could not find page or resource</p>
+        </main>
+    )
+}
