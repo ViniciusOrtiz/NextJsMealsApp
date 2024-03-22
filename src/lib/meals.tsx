f2c4d09b21e5ee93d9e76a6616895cbb5442cfd6
Vinicius Ortiz <vinicius-ortiz@outlook.com>
2024-03-21 23:12:14 -0300

    sanitize meal data

--- src/lib/meals.tsx
+++ src/lib/meals.tsx
@@ -1,4 +1,6 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -10,4 +12,20 @@ export async function getMeals() {
 
 export async function getMeal(slug: any) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export function saveMeal(meal: any) {
+    const slug = slugify(meal.title, { lower: true });
+    const stmt = db.prepare(
+        'INSERT INTO meals (slug, creator, creator_email, title, summary, instructions, image) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    );
+    stmt.run(
+        slug,
+        xss(meal.creator),
+        xss(meal.creator_email),
+        xss(meal.title),
+        xss(meal.summary),
+        xss(meal.instructions),
+        xss(meal.image)
+    );
 }
